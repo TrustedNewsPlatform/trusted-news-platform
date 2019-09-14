a8f734f51824fff8dd7b89c117e69fdab90dc181
Piotr Adamczyk <xeno@xeno.yt>
2019-09-14 16:39:47 +0200

    trune header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TruneLogo from "./resources/logo.png";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Navbar expand="sm">
+            <Navbar.Brand href="#home">
+              <img
+                src={TruneLogo}
+                height="22"
+                style={{ marginTop: 4, marginBottom: 4 }}
+                className="d-inline-block align-top"
+                alt="Trune logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Enter news domain..."
+                  className="mr-sm-2"
+                />
+                <Button variant="outline-primary">Show</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
