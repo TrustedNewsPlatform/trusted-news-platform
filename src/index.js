9492b5aa77cc43869be6dde9aa6688ca233a8e26
Piotr Adamczyk <xeno@xeno.yt>
2019-09-15 11:38:13 +0200

    basename change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/projekty/trune">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
