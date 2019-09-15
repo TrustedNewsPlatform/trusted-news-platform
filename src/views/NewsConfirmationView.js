b1a7701e914f163813752c36371ae8c7e2249259
Marcello Bardus <marcellobardus8@gmail.com>
2019-09-15 10:30:11 +0200

    confirmation view update

--- src/views/NewsConfirmationView.js
+++ src/views/NewsConfirmationView.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, ButtonToolbar } from "react-bootstrap";
 
 import Swal from "sweetalert2";
 
 import { contract, provider, signerAvailable } from "../utils/ethereum";
 import { ethers } from "ethers";
 
+import { ipfsEndpoint } from "../config";
+import { bytes32ToIpfsHash, fetchWithTimeout } from "../utils/ipfsUtils";
+
+import ReactMarkdown from "react-markdown";
+
+import { Link } from "react-router-dom";
+
 export class NewsConfirmationView extends Component {
   state = {
     allNews: [],
     notConfirmedNews: [],
     loaded: false,
-    newsCount: null
+    newsCount: null,
+    contents: {}
   };
 
-  async componentDidMount() {
+  async getContractData() {
     if (!signerAvailable) return this.setState({ loaded: true });
     const address = await provider.getAddress();
     const newsCount = await contract.getNewsConceringCount(address);
@@ -30,25 +38,34 @@ export class NewsConfirmationView extends Component {
         this.state.allNews[i],
         address
       );
-      const remainingApprovals = await contract.getNewsRemainingApprovals(
-        this.state.allNews[i]
-      );
-      if (!isNewsConfirmed && remainingApprovals !== 0) {
+      const didVoteFor = await contract.didVote(this.state.allNews[i], address);
+      if (!isNewsConfirmed && !didVoteFor) {
         this.setState({
           notConfirmedNews: [
             ...this.state.notConfirmedNews,
             this.state.allNews[i]
           ]
         });
+        const ipfsHash = await bytes32ToIpfsHash(this.state.allNews[i]);
+        const content = await fetchWithTimeout(ipfsEndpoint + ipfsHash).then(
+          res => res.text()
+        );
+        let stateContents = this.state.contents;
+        stateContents[this.state.allNews[i]] = content;
+        this.setState({ contents: stateContents });
       }
     }
     this.setState({ loaded: true });
   }
 
+  async componentDidMount() {
+    await this.getContractData();
+  }
+
   async handleApprove(ipfsHash) {
     await contract.approveNews(ipfsHash);
   }
-  async handleDisapprove(ipfsHash, explanationHash) {
+  async handleDisapprove(ipfsHash) {
     Swal.fire({
       title: "Please give a reason",
       input: "text",
@@ -59,10 +76,6 @@ export class NewsConfirmationView extends Component {
       confirmButtonText: "Submit",
       showLoaderOnConfirm: true,
       preConfirm: async explanation => {
-        window.localStorage.setItem(
-          "trune" + ipfsHash + "HasBeenDisapproved",
-          true
-        );
         await contract.disapproveNews(
           ipfsHash,
           ethers.utils.sha256(Buffer(explanation))
@@ -88,17 +101,40 @@ export class NewsConfirmationView extends Component {
               <div>
                 {this.state.notConfirmedNews.map(hash => (
                   <div>
-                    {window.localStorage.getItem(
-                      "trune" + hash + "HasBeenDisapproved"
-                    ) ? null : (
-                      <div>
-                        <Card style={{ width: "18rem" }}>
-                          <Card.Body>
-                            <Card.Title>Some name</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">
-                              {hash}
-                            </Card.Subtitle>
-                          </Card.Body>
+                    <div>
+                      <Card
+                        style={{
+                          width: "100%",
+                          marginLeft: "10px",
+                          marginRight: "10px"
+                        }}
+                      >
+                        <Card.Body>
+                          <Card.Title>
+                            <ReactMarkdown
+                              source={
+                                this.state.contents[hash]
+                                  .replace(/!\[(.*)\]\(.*\)/g, "`$1`")
+                                  .split("\n")[0]
+                              }
+                            />
+                          </Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">
+                            <Link to={"/article/" + hash}>
+                              {"Read more..."}
+                            </Link>
+                          </Card.Subtitle>
+                        </Card.Body>
+                        <ButtonToolbar style={{ padding: 10 }}>
+                          <Button
+                            variant="danger"
+                            style={{ marginLeft: "5px", marginRight: "5px" }}
+                            onClick={async () => {
+                              await this.handleDisapprove(hash);
+                            }}
+                          >
+                            Decline
+                          </Button>
                           <Button
                             variant="success"
                             style={{ marginLeft: "5px", marginRight: "5px" }}
@@ -108,20 +144,10 @@ export class NewsConfirmationView extends Component {
                           >
                             Approve
                           </Button>
-                          <br />
-                          <Button
-                            variant="danger"
-                            style={{ marginLeft: "5px", marginRight: "5px" }}
-                            onClick={async () => {
-                              await this.handleDisapprove(hash, "");
-                            }}
-                          >
-                            Decline
-                          </Button>
-                        </Card>
-                        <br />
-                      </div>
-                    )}
+                        </ButtonToolbar>
+                      </Card>
+                      <br />
+                    </div>
                   </div>
                 ))}
               </div>
