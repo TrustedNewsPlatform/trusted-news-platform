6473cee03d5adb77b23185c1e6c9a23637c6ef8e
Piotr Adamczyk <xeno@xeno.yt>
2019-09-15 04:54:21 +0200

    removed ipfs debug

--- src/utils/ipfsUtils.js
+++ src/utils/ipfsUtils.js
@@ -14,12 +14,6 @@ const bytes32ToIpfsHash = bytes32 => {
   return bs58.encode(Buffer.from("1220" + bytes32.substr(2), "hex"));
 };
 
-console.log(
-  bytes32ToIpfsHash(
-    "0xf168e785c4a50c0b8dbd82a6cf0388dc4ed4f4f0044ee02a80d44fa3eb7edea8"
-  )
-);
-
 const fetchWithTimeout = (url, options, timeout = 7000) => {
   return Promise.race([
     fetch(url, options),
